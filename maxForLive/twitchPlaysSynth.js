913f27eb9248f54c286534cfcafea50f353a28ce
Matthew Dunlap <matthew@dunlap.solutions>
2021-02-22 23:41:35 -0500

    added twitch login
    creds are no longer hard-coded; users enter the twitch api creds into the settings tab on tpts.main.amxd live device; resolves #19

--- maxForLive/twitchPlaysSynth.js
+++ maxForLive/twitchPlaysSynth.js
@@ -19,15 +19,22 @@ const max = require('max-api');
 // https://github.com/tmijs/docs/tree/gh-pages/_posts/v1.4.2
 const tmi = require('tmi.js');
 
+// store twitch api connection settings in this data struct
+// DO NOT FILL IN WITH YOUR SETTINGS
+// this will be populated in the MFL patch via the twitchSettings ID key
+twitchApiOptions = {
+    identity: {
+        username: "",
+        password: ""
+    },
+    channels: [ "" ]
+};
+
 // create Twitch client
-const client = new tmi.client(creds.opts);
+const client = new tmi.client();
 
 // Register our event handlers (defined below)
 client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
-
-// Connect to Twitch:
-client.connect();
 
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
@@ -64,11 +71,6 @@ function onMessageHandler (target, context, msg, self) {
     }
 }
 
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-    max.outlet(`twitchMessage Connected to ${addr}:${port}`);
-}
-
 // Called when a user joins or parts the channel
 function onJoinHandler (channel, username, self) {
     // if accumulator is less than one, start recording
@@ -85,9 +87,58 @@ function onPartHandler (channel, username, self) {
 
 }
 
+// parse message to connect to Twitch (message === "twitchConnect")
+max.addHandler("twitchConnect", () => {
+    // fill in the client connection settings
+    client.username = twitchApiOptions.identity.username;
+    client.password = twitchApiOptions.identity.password;
+    client.channels = twitchApiOptions.channels;
+
+    // connect bot to twitch
+    client.connect()
+        .then((data) => {
+            max.outlet(`twitchMessage Connected to ${data}`);
+        })
+        .catch((err) => {
+            max.outlet(`twitchMessage Error connecting to Twitch: ${err}`);
+            max.outlet("twitchBangDisconnectError bang");
+        });
+});
+
+// parse message to disconnect from Twitch api ("twitchDisconnect")
+max.addHandler("twitchDisconnect", () => {
+    // connect bot to twitch
+    client.disconnect();
+    max.outlet("twitchMessage Disconnected from Twitch");
+});
+
+// parse twitch api access settings (prepended with twitchSettings)
+max.addHandler("twitchSettings", (inputMessage) => {
+    // find the setting type by key
+    let settingsKey = inputMessage.match(/^\w+/);
+    let keyPlusSpaceRegex = /^\w+\s/;
+
+    // twitchApi
+    if(settingsKey[0] === "twitchApi"){
+        twitchApiOptions.identity.password = inputMessage.replace(keyPlusSpaceRegex, "");
+    }
+
+    // twitchUser
+    if(settingsKey[0] === "twitchUser"){
+        twitchApiOptions.identity.username = inputMessage.replace(keyPlusSpaceRegex, "");
+    }
+
+    // twitchChan
+    if(settingsKey[0] === "twitchChan"){
+        twitchApiOptions.channels[0] = inputMessage.replace(keyPlusSpaceRegex, "");
+    }
+});
+
+
 // parse messages sent from the inlet (for testing commands)
 max.addHandler("input", (inputMessage) => {
 
+    // parse through command parser if prepended with "input"
     let outputMessage = parser.parse(inputMessage);
 
     // output all parsed Max messages in returned array
